b2d955bd74b4a0e339fd4078f8bdd8f4645439c1
Alex Altieri & Abdul Rahimi <student@epicodus.com>
2015-09-24 11:54:57 -0700

    add delete and update fuctionality

--- app/components/post-canvas.js
+++ app/components/post-canvas.js
@@ -4,6 +4,10 @@ export default Ember.Component.extend({
   actions: {
     save(post, params) {
       this.sendAction('save', post, params)
+    },
+
+    update(post, params) {
+      this.sendAction('update', post, params);
     }
   }
 });
---
+++ app/components/update-post.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updatePostForm: false,
+
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+
+    delete(post){
+      if (confirm('Are you sure you want to delete this post?')){
+        this.sendAction('destroyPost', post);
+      }
+    },
+
+    update(post){
+      var params = {
+        title: this.get('title'),
+        date: this.get('date'),
+        author: this.get('author'),
+        image: this.get('image'),
+        content: this.get('content')
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
+});
